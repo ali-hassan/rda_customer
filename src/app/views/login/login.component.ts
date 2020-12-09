71e5faad3e8f4a2ef259a748283a7400e622e641
Rashed Mehmood <rashid.mehmood@systemslted.com>
2020-12-09 13:59:33 +0500

    Add validation on login fields

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
@@ -8,9 +10,12 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   templateUrl: 'login.component.html'
 
 })
-export class LoginComponent {
-  loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
-  });
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+  }
 }
