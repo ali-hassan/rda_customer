f3c329bafc79fb731736552c99d72e97fd6b26a1
Rashed Mehmood <rashid.mehmood@systemslted.com>
2020-12-09 21:06:11 +0500

    Add validation on registration page

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CustomvalidationService} from '../../services/customvalidation.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +8,30 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   protected aFormGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  submitted = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) { }
   ngOnInit() {
     this.aFormGroup = this.formBuilder.group({
-      recaptcha: ['', Validators.required]
+      recaptcha:    ['', Validators.required],
+      fullName:     ['', Validators.required],
+      currency:     ['', Validators.required],
+      cnic:         ['', Validators.required],
+      email:        ['', Validators.required],
+      accountType:  ['', Validators.required]
     });
+  }
+  get registerFormControl() {
+    return this.aFormGroup.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.aFormGroup.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.aFormGroup.value);
+    }
   }
   showOtp() {
     document.getElementById('otp').style.display = 'block';
