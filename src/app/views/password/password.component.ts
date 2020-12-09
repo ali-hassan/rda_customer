b73169a1436fc957c48f24d5f8d8fbd6e0ac5b3a
Rashed Mehmood <rashid.mehmood@systemslted.com>
2020-12-09 18:35:08 +0500

    Add validation on password component

--- src/app/views/password/password.component.ts
+++ src/app/views/password/password.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { CustomvalidationService } from '../../services/customvalidation.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,31 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class PasswordComponent implements OnInit {
   protected aFormGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  submitted = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) { }
   ngOnInit() {
     this.aFormGroup = this.formBuilder.group({
-      recaptcha: ['', Validators.required]
-    });
+      recaptcha:          ['', Validators.required],
+        password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validator: this.customValidator.MatchPassword('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  get registerFormControl() {
+    return this.aFormGroup.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.aFormGroup.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.aFormGroup.value);
+    }
   }
 }
